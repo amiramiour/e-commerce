27e5e9af781eb4d9dd18955af699f159d334bb75
SarahOtmane <sarahotmane02@gmail.com>
2024-10-28 15:13:49 +0100

    initalisation et lancement de app

---
+++ service-paiment/api/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const connectDB = require('./config/connectDb');
+const configureServices = require('./config/configureServices');
+const startServeur = require('./config/startServeur');
+
+const app = express();
+
+connectDB();
+configureServices(app);
+startServeur(app);
+
+export default app;

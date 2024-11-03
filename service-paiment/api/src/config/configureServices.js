ff4d232178f98a765758c5ad4b5c12a81a140e4a
SarahOtmane <sarahotmane02@gmail.com>
2024-11-03 10:36:07 +0100

    add checkoutRoute

--- service-paiment/api/src/config/configureServices.js
+++ service-paiment/api/src/config/configureServices.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const  swaggerUi = require('swagger-ui-express');
 const configSwagger =  require('../config/swagger-config');
+const checkoutRoute = require('../routes/checkoutRoute');
 
 const configureServices = (app) => {
   // Swagger documentation route
@@ -9,6 +10,9 @@ const configureServices = (app) => {
   // Middleware for parsing requests
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+
+  // Checkout route
+  app.use('/', checkoutRoute);
 };
 
 module.exports = configureServices;

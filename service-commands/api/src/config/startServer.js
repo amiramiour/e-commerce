471a48001d016f65f0398aee55278dc6e03249cb
aminelhm <131161302+aminelhm@users.noreply.github.com>
2024-11-05 03:40:27 +0100

    Création de l’arborescence de dossiers pour le service de gestion des commandes avec Docker

--- service-commands/api/src/config/startServer.js
+++
@@ -1,9 +0,0 @@
-const startServer = (app) => {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  };
-  
-  export default startServer;
-  

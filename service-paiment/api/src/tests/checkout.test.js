33aa80d06d49d96efc8d52ec9fdf5bd69c743c86
SarahOtmane <sarahotmane02@gmail.com>
2024-11-08 09:18:58 +0100

    test file for paiment controller

---
+++ service-paiment/api/src/tests/checkout.test.js
@@ -0,0 +1,94 @@
+const supertest = require('supertest');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const configureServices = require('../config/configureServices');
+const dotenv = require('dotenv');
+const { connectDb, sequelize } = require('../config/connectDb');
+const PaimentModel = require('../models/paimentModel');
+dotenv.config()
+
+
+describe('Checkout controller', () => {
+    let token;
+    const app = express();
+
+    beforeAll(async() => {
+        configureServices(app);
+        await connectDb();
+        await sequelize.destroyAll();
+
+        //Génération du token
+        token = jwt.sign({ id: 1, role: 1 }, process.env.JWT_KEY, { expiresIn: '1h' });
+    });
+
+    afterAll(async () => {
+        await sequelize.destroyAll();
+        await sequelize.close();
+    });
+
+    describe('POST /create-checkout-session', () => {
+        it('should return 200 and create a checkout session', async () => {
+            const response = await supertest(app)
+                .post('/create-checkout-session')
+                .set('Authorization', token)
+                .send({
+                    products: [
+                        {
+                            name: 'Produit 1',
+                            description: 'Description du produit 1',
+                            price: 10,
+                            quantity: 1
+                        }
+                    ]
+                });
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body.id).toBeDefined();
+
+            const paymentLog = await PaimentModel.findOne({ where: { sessionId: response.body.id } });
+            expect(paymentLog).not.toBeNull();
+            expect(paymentLog.userId).toBe(1);
+            expect(paymentLog.status).toBe('created');
+            expect(paymentLog.amount_total).toBe(1000);
+            expect(paymentLog.currency).toBe('eur');
+        });
+        
+        it('should return 403 if the token is missing', async () => {
+            const response = await supertest(app)
+                .post('/create-checkout-session')
+                .send({
+                    products: [
+                        {
+                            name: 'Produit 1',
+                            description: 'Description du produit 1',
+                            price: 10,
+                            quantity: 1
+                        }
+                    ]
+                });
+
+            expect(response.statusCode).toBe(403);
+            expect(response.body.message).toBe('Accès interdit: token manquant');
+        });
+
+        it('should return 403 if the token is invalid', async () => {
+            const response = await supertest(app)
+                .post('/create-checkout-session')
+                .set('Authorization', 'invalidtoken')
+                .send({
+                    products: [
+                        {
+                            name: 'Produit 1',
+                            description: 'Description du produit 1',
+                            price: 10,
+                            quantity: 1
+                        }
+                    ]
+                });
+
+            expect(response.statusCode).toBe(403);
+            expect(response.body.message).toBe('Accès interdit: token invalide');
+        });
+    });
+    
+});

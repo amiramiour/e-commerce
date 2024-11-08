90f6fca82a44b4cf75ec5142c48bebebff7a5fcd
SarahOtmane <sarahotmane02@gmail.com>
2024-11-08 11:23:00 +0100

    add function to start service

--- service-paiment/api/src/app.js
+++ service-paiment/api/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { connectDB } = require('./config/connectDb');
+const {connectDB} = require('./config/connectDb');
 const configureServices = require('./config/configureServices');
 const startServeur = require('./config/startServeur');
 
 const app = express();
 
-connectDB();
-configureServices(app);
-startServeur(app);
+const start = async () => {
+    await connectDB();
+    configureServices(app);
+    startServeur(app);
+}
+
+start();

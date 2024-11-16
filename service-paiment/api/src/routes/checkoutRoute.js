ec5154bfe557c2d0c113670ee9725942d9b2834e
SarahOtmane <sarahotmane02@gmail.com>
2024-11-16 17:52:04 +0100

    supp la gestion du token

--- service-paiment/api/src/routes/checkoutRoute.js
+++ service-paiment/api/src/routes/checkoutRoute.js
@@ -1,9 +1,8 @@
 const Router = require('express');
 const checkoutController = require('../controllers/checkoutController');
-const jwtMiddleware = require('../middleware/jwtMiddleware');
 
 const router = Router();
 
-router.post('/create-checkout-session', jwtMiddleware.verifyToken, checkoutController.createCheckout);
+router.post('/create-checkout-session', checkoutController.createCheckout);
 
 module.exports = router;

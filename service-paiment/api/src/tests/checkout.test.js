cc4b0f643079f0b16ee7c0a160a341ac15a8a535
SarahOtmane <sarahotmane02@gmail.com>
2024-11-16 17:58:49 +0100

    supp les tests concernants le token

--- service-paiment/api/src/tests/checkout.test.js
+++ service-paiment/api/src/tests/checkout.test.js
@@ -9,16 +9,12 @@ dotenv.config()
 
 
 describe('Checkout controller', () => {
-    let token;
     const app = express();
 
     beforeAll(async() => {
         configureServices(app);
         await connectDB();
         await db.sync({ force: true });
-
-        //Génération du token
-        token = jwt.sign({ id: 1, role: 1 }, process.env.JWT_KEY, { expiresIn: '1h' });
     });
 
     afterAll(async () => {
@@ -30,7 +26,6 @@ describe('Checkout controller', () => {
         it('should return 200 and create a checkout session', async () => {
             const response = await supertest(app)
                 .post('/create-checkout-session')
-                .set('Authorization', `Bearer ${token}`)
                 .send({
                     products: [
                         {
@@ -52,43 +47,6 @@ describe('Checkout controller', () => {
             expect(paymentLog.amount_total).toBe(1000);
             expect(paymentLog.currency).toBe('eur');
         });
-        
-        // it('should return 403 if the token is missing', async () => {
-        //     const response = await supertest(app)
-        //         .post('/create-checkout-session')
-        //         .send({
-        //             products: [
-        //                 {
-        //                     name: 'Produit 1',
-        //                     description: 'Description du produit 1',
-        //                     price: 10,
-        //                     quantity: 1
-        //                 }
-        //             ]
-        //         });
-
-        //     expect(response.statusCode).toBe(403);
-        //     expect(response.body.message).toBe('Accès interdit: token manquant');
-        // });
-
-        // it('should return 403 if the token is invalid', async () => {
-        //     const response = await supertest(app)
-        //         .post('/create-checkout-session')
-        //         .set('Authorization', 'invalidtoken')
-        //         .send({
-        //             products: [
-        //                 {
-        //                     name: 'Produit 1',
-        //                     description: 'Description du produit 1',
-        //                     price: 10,
-        //                     quantity: 1
-        //                 }
-        //             ]
-        //         });
-
-        //     expect(response.statusCode).toBe(403);
-        //     expect(response.body.message).toBe('Accès interdit: token invalide');
-        // });
     });
     
 });

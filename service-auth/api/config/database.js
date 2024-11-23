8812d6c7cc59ef353f751f56f33e3bdf3a9283a7
SarahOtmane <sarahotmane02@gmail.com>
2024-11-23 13:19:43 +0100

    update le host de la db du service auth

--- service-auth/api/config/database.js
+++ service-auth/api/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
     {
-        host: process.env.DB_HOST || "db",
+        host: process.env.DB_HOST || "auth-db",
         dialect: "mysql",
         port: process.env.DB_PORT || 3306,
         retry: {

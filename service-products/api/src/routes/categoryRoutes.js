02907c42162e808d2840cdb291f0988b313ba9a2
SarahOtmane <sarahotmane02@gmail.com>
2024-11-25 23:16:10 +0100

    update des routes du crud categories

--- service-products/api/src/routes/categoryRoutes.js
+++ service-products/api/src/routes/categoryRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const categoryController = require('../controllers/CategoryController');
 
 // Routes pour les catégories
-router.get('/', categoryController.getAllCategories);
-router.post('/', categoryController.createCategory);
-router.put('/:id', categoryController.updateCategory);
-router.delete('/:id', categoryController.deleteCategory);
+router.get('/list', categoryController.getAllCategories);
+
+router.post('/admin', categoryController.createCategory);
+router.put('/admin/:id', categoryController.updateCategory);
+router.delete('/admin/:id', categoryController.deleteCategory);
 
 module.exports = router;

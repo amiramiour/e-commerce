74f02c85ace27a6eb7f50bc43cd239a5e7bdf607
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 00:54:35 +0100

    sync order model

--- service-commands/api/src/config/configureServices.js
+++ service-commands/api/src/config/configureServices.js
@@ -2,20 +2,23 @@ const express = require('express');
 const  swaggerUi = require('swagger-ui-express');
 const configSwagger =  require('../config/swagger-config');
 const ordersRoutes = require('../routes/ordersRoutes');
+const Order = require('../models/orderModel');
 
 const configureServices = async(app) => {
-  // Swagger documentation route
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(configSwagger));
+    // Swagger documentation route
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(configSwagger));
 
-  // Middleware for parsing requests
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+    // Middleware for parsing requests
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-  app.use('/api', ordersRoutes);
+    app.use('/api', ordersRoutes);
 
-  app.get('/', (req, res) => {
-    res.send('Service de gestion des commandes est opérationnel');
-});
+    app.get('/', (req, res) => {
+        res.send('Service de gestion des commandes est opérationnel');
+    });
+    
+    await Order.sync();
 };
 
 module.exports = configureServices;

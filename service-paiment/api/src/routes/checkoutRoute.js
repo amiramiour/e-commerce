cbb00ddb2890bab3d821ac675f0a100861317bf7
SarahOtmane <sarahotmane02@gmail.com>
2024-11-25 23:48:41 +0100

    update checkout route

--- service-paiment/api/src/routes/checkoutRoute.js
+++ service-paiment/api/src/routes/checkoutRoute.js
@@ -3,7 +3,7 @@ const checkoutController = require('../controllers/checkoutController');
 
 const router = express.Router();
 
-router.post('/create-checkout-session', checkoutController.createCheckout);
+router.post('/:id/create-checkout-session', checkoutController.createCheckout);
 router.post('/webhook', express.raw({ type: 'application/json' }), checkoutController.manageWebhook);
 
 module.exports = router;

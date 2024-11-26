3dc84435c03e28d1860add06d493e77be1126d45
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 16:26:33 +0100

    add userRegister and userLogin

--- api-gateway/api/src/controllers/userController.js
+++ api-gateway/api/src/controllers/userController.js
@@ -2,7 +2,7 @@ const axios = require('../middlewares/axiosConfig');
 
 exports.getUserById = async (req, res) => {
     try {
-        await axios.axiosUser.get(`/users/${req.user.id}`).then((response) => {
+        await axios.axiosUser.get(`/${req.user.id}`).then((response) => {
             res.status(response.status).json(response.data);
         });
 
@@ -14,7 +14,7 @@ exports.getUserById = async (req, res) => {
     
 exports.updateUser = async (req, res) => {
     try {
-        await axios.axiosUser.put(`/users/${req.user.id}`, req.body).then((response) => {
+        await axios.axiosUser.put(`/${req.user.id}`, req.body).then((response) => {
             res.status(response.status).json(response.data);
         });
 
@@ -26,11 +26,35 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
     try {
-        await axios.axiosUser.delete(`/users/${req.user.id}`).then((response) => {
+        await axios.axiosUser.delete(`/${req.user.id}`).then((response) => {
             res.status(response.status).json(response.data);
         });
 
     } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+}
+
+exports.userRegister = async (req, res) => {
+    try {
+        await axios.axiosAuth.post('/register', req.body).then((response) => {
+            res.status(response.status).json(response.data);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur serveur' });
+        
+    }
+}
+
+exports.userLogin = async (req, res) => {
+    try {
+        await axios.axiosAuth.post('/login', req.body).then((response) => {
+            res.status(response.status).json(response.data);
+        });
+    }
+    catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Erreur serveur' });
     }

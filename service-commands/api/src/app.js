58b584451fbe1bc9e2e44619e7e0f592212ecb9b
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 14:47:26 +0100

    update path of startserveur

--- service-commands/api/src/app.js
+++ service-commands/api/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {connectDB} = require('./config/connectDb');
 const configureServices = require('./config/configureServices');
-const startServeur = require('./config/startServeur');
+const startServeur = require('./config/startServer');
 
 const app = express();
 

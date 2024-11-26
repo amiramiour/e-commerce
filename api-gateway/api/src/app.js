3a9a55aab6456ae589319ab175e6eac9827333fe
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 15:46:02 +0100

    startserveur

--- api-gateway/api/src/app.js
+++ api-gateway/api/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const configureServices = require('./services/configureService');
-const configureRoutes = require('./services/startService');
+const startServices = require('./services/startService');
 
 const app = express();
 configureServices(app);
-configureRoutes(app);
+startServices(app);

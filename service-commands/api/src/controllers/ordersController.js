daaa0d8e8d002aee47375e5ddb1beea3d858f63d
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 09:38:27 +0100

    remove update and delete order

--- service-commands/api/src/controllers/ordersController.js
+++ service-commands/api/src/controllers/ordersController.js
@@ -5,7 +5,7 @@ exports.createOrder = async (req, res) => {
     const { product_id, quantity, total_price } = req.body;
     try {
         // Création de la commande dans la base de données
-        const order = await Order.create({ product_id, quantity, total_price });
+        const order = await Order.create({ product_id, quantity, total_price, user_id: req.params.id_user });
         res.status(201).json({ message: 'Commande créée avec succès', data: order });
     } catch (error) {
         res.status(500).json({ message: 'Erreur lors de la création de la commande', error: error.message });
@@ -39,38 +39,18 @@ exports.getOrderById = async (req, res) => {
     }
 };
 
-// Mettre à jour une commande par ID
-exports.updateOrder = async (req, res) => {
+// Récupérer une commande par ID
+exports.getOrdersOfAUser = async (req, res) => {
     const { id } = req.params;
-    const { product_id, quantity, total_price } = req.body;
     try {
-        // Mise à jour de la commande
-        const [updated] = await Order.update(
-            { product_id, quantity, total_price },
-            { where: { id } }
-        );
-        if (updated) {
-            res.status(200).json({ message: 'Commande mise à jour avec succès' });
+        // Recherche de la commande par son ID
+        const orders = await Order.findAll({ where: { user_id: id_user } });
+        if (orders) {
+            res.status(200).json(orders);
         } else {
             res.status(404).json({ message: 'Commande non trouvée' });
         }
     } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la mise à jour de la commande', error: error.message });
-    }
-};
-
-// Supprimer une commande par ID
-exports.deleteOrder = async (req, res) => {
-    const { id } = req.params;
-    try {
-        // Suppression de la commande
-        const deleted = await Order.destroy({ where: { id } });
-        if (deleted) {
-            res.status(200).json({ message: 'Commande supprimée avec succès' });
-        } else {
-            res.status(404).json({ message: 'Commande non trouvée' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la suppression de la commande', error: error.message });
+        res.status(500).json({ message: 'Erreur lors de la récupération de la commande', error: error.message });
     }
 };

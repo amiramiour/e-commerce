6f628b190540fa80d732d550dbcbdfd3e3ee9dee
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 16:26:42 +0100

    add route of userLogin and userLogin

--- api-gateway/api/src/routes/userRoute.js
+++ api-gateway/api/src/routes/userRoute.js
@@ -7,5 +7,7 @@ const jwtMiddleware = require('../middlewares/jwtMiddleware');
 router.get('/', jwtMiddleware.verifyTokenUser, userController.getUserById );
 router.put('/', jwtMiddleware.verifyTokenUser, userController.updateUser );
 router.delete('/', jwtMiddleware.verifyTokenUser, userController.deleteUser );
+router.post('/register', userController.userRegister );
+router.post('/login', userController.userLogin);
 
 module.exports = router;

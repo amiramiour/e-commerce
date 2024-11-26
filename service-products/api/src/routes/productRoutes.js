d33f2472910e23a8de0302f74064bdf2aad62c9d
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 13:59:05 +0100

    add route de getProductByName

--- service-products/api/src/routes/productRoutes.js
+++ service-products/api/src/routes/productRoutes.js
@@ -7,6 +7,7 @@ router.get('/list', productController.getAllProducts);
 router.post('/admin', productController.createProduct); 
 router.put('/admin/:id', productController.updateProduct); 
 router.delete('/admin/:id', productController.deleteProduct); 
+router.get('/admin/:name', productController.getProductByName);
 router.get('/list/:id/similar', productController.getSimilarProducts);
 
 module.exports = router;

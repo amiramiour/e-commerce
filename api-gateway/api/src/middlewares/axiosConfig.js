d85479db377cc7e93f07ea5007d306b0aba8c677
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 09:40:21 +0100

    add axiosOrder

--- api-gateway/api/src/middlewares/axiosConfig.js
+++ api-gateway/api/src/middlewares/axiosConfig.js
@@ -8,6 +8,11 @@ exports.axiosProduct = axios.create({
     baseURL: 'http://localhost:3003'
 });
 
+exports.axiosOrder = axios.create({
+    baseURL: 'http://localhost:3004'
+});
+
+
 exports.axiosPaiment = axios.create({
     baseURL: 'http://localhost:3005'
 });

f7dbd1c856139539648c984b099f40c86e7a3ad9
SarahOtmane <sarahotmane02@gmail.com>
2024-11-26 16:26:17 +0100

    add axios auth

--- api-gateway/api/src/middlewares/axiosConfig.js
+++ api-gateway/api/src/middlewares/axiosConfig.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
+exports.axiosAuth = axios.create({
+    baseURL: 'http://localhost:3001/auth'
+});
+
 exports.axiosUser = axios.create({
-    baseURL: 'http://localhost:3002'
+    baseURL: 'http://localhost:3002/users'
 });
 
 exports.axiosProduct = axios.create({

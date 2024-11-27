acd07caf7c0af0149b44150144fc38377e17dff2
SarahOtmane <sarahotmane02@gmail.com>
2024-11-27 13:27:47 +0100

    update params of route of service commands

--- service-commands/api/src/routes/ordersRoutes.js
+++ service-commands/api/src/routes/ordersRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ordersController = require('../controllers/ordersController');
 
 // Route pour créer une nouvelle commande
-router.post('/orders/id_user', ordersController.createOrder);
+router.post('/orders/:id_user', ordersController.createOrder);
 
 // Route pour récupérer toutes les commandes
 router.get('/orders', ordersController.getAllOrders);

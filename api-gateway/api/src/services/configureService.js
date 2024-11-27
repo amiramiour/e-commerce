ad3a71dcbf6a8f567d91d8df8ed2fc3ae6bd48d9
SarahOtmane <sarahotmane02@gmail.com>
2024-11-27 12:00:13 +0100

    definir les routes dans l api gateway

--- api-gateway/api/src/services/configureService.js
+++ api-gateway/api/src/services/configureService.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const cors = require('cors');
+
 const userRoute = require('../routes/userRoute');
 const paimentRoute = require('../routes/paimentRoute');
+const categoryRoute = require('../routes/categoryRoute');
+const productRoute = require('../routes/productRoutes');
+const orderRoute = require('../routes/orderRoute');
+const sizeRoute = require('../routes/sizeRoute');
+const colorRoute = require('../routes/colorRoute');
 
 const configureServices = async(app) => {
     // Middleware for parsing requests
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    //CRUD pour les users 
+    app.use(cors());
+
+    console.log('TTTTTTTTTTTTTTT')
+
+
     app.use('/users', userRoute); 
-    // create checkout session
     app.use('/paiment', paimentRoute);
+    app.use('/category', categoryRoute);
+    app.use('/product', productRoute);
+    app.use('/order', orderRoute);
+    app.use('/size', sizeRoute);
+    app.use('/color', colorRoute);
 
     app.get('/', (req, res) => {
         res.json({ message: 'API Gateway is running!' });
